6eec80c3aad391c4eefd754142d338233d5a43b6
Sandeep <sandeep06011991@gmail.com>
2015-12-28 16:45:26 +0530

    stat

--- workoutplan/urls.py
+++ workoutplan/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^home/',homePage),
     url(r'^browse/(?P<id>[-\d]+)',browse),
    url(r'^api/search/',searchAPI),
